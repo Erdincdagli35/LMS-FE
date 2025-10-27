9f67839cd10f9ce4cbdf940320c48d0b294ed9ad
erdindagli35 <36699257+Erdincdagli35@users.noreply.github.com>
2025-10-27 10:37:16 +0300

    Fix base URL and addToShelf URL in ShelfService

--- src/app/shelf-service/shelf-service.ts
+++ src/app/shelf-service/shelf-service.ts
@@ -9,8 +9,8 @@ import { Shelf } from '../models/shelf';
   providedIn: 'root'
 })
 export class ShelfService {
-  private baseURL = "https://lms-be.up.railway.app/shelves";
-  private addToShelfURL = "https://lms-be.up.railway.app/shelves/addBook";
+  private baseURL = "https://lmss-be.up.railway.app/shelves";
+  private addToShelfURL = "https://lmss-be.up.railway.app/shelves/addBook";
 
   constructor(private httpClient: HttpClient) { }
 
